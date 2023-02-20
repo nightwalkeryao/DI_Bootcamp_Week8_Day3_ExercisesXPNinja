97f6883f5a54313277e69c5f5bdda79442af003a
NightWalker Yao <huntr@pm.me>
2023-02-20 18:10:34 +0000

    Update SQL script

--- Exercise.sql
+++ Exercise.sql
@@ -1,32 +1,62 @@
---Retrieve all films with a rating of G or PG, which are are not currently rented (they have been returned/have never been borrowed).
-
-SELECT film.film_id, film.title, film.rating
+--- Exercise 1 : DVD Rentals
+-- 1) Retrieve all films with a rating of G or PG, 
+	-- which are are not currently rented (they have been returned/have never been borrowed).
+SELECT DISTINCT(inventory.film_id), film.*
    FROM film
-   LEFT JOIN inventory ON film.film_id = inventory.film_id
-   LEFT JOIN rental ON inventory.inventory_id = rental.inventory_id
-   WHERE film.rating IN ('G', 'PG') AND rental.inventory_id IS NULL;
+   INNER JOIN inventory ON inventory.film_id = film.film_id
+   WHERE film.rating='G' OR film.rating = 'PG'
+   AND film.film_id NOT IN (SELECT rental.inventory_id FROM rental);
 
-
---Create a new table which will represent a waiting list for children’s movies. This will allow a child to add their name to the list until the DVD is available (has been returned). Once the child takes the DVD, their name should be removed from the waiting list (ideally using triggers, but we have not learned about them yet. Let’s assume that our Python program will manage this). Which table references should be included?
-
-CREATE TABLE children_waiting_list (
-   waiting_id SERIAL PRIMARY KEY,
-   film_id INT NOT NULL,
-   customer_id INT NOT NULL,
-   FOREIGN KEY (film_id) REFERENCES film (film_id),
-   FOREIGN KEY (customer_id) REFERENCES customer (customer_id)
+/*
+	1) Create a new table which will represent a waiting list for children’s movies. 
+	This will allow a child to add their name to the list until the DVD is available 
+	(has been returned). Once the child takes the DVD, their name should be 
+	removed from the waiting list (ideally using triggers, but we have not 
+	learned about them yet. Let’s assume that our Python program will manage this). 
+	Which table references should be included?
+*/
+CREATE TABLE waiting_list(
+	id SERIAL PRIMARY KEY,
+	complete_name VARCHAR NOT NULL,
+	inventory_id INTEGER NOT NULL,
+	takes BOOLEAN DEFAULT FALSE,
+	CONSTRAINT fk_inventory
+		FOREIGN KEY(inventory_id)
+		REFERENCES inventory(inventory_id)
+		ON UPDATE CASCADE 
+		ON DELETE RESTRICT
 );
 
+CREATE OR REPLACE FUNCTION fn_waiting_list() 
+   RETURNS TRIGGER 
+   LANGUAGE PLPGSQL
+AS 
+'
+BEGIN
+	IF NEW.takes THEN
+		DELETE FROM waiting_list WHERE id = NEW.id; 
+	END IF;
+   
+   RETURN NULL;
+END;
+'
 
---insertion
+CREATE TRIGGER tr_waiting_list
+   AFTER UPDATE
+   ON waiting_list
+   FOR EACH ROW
+       EXECUTE PROCEDURE fn_waiting_list();
+       
 
-INSERT INTO children_waiting_list (film_id, customer_id)
-   VALUES ((SELECT film_id FROM film WHERE title = 'Boiled Dares'), 1),
-         ((SELECT film_id FROM film WHERE title = 'Born Spinal'), 2);
-
-
-SELECT film.title, COUNT(*) AS waiting_count
-   FROM children_waiting_list
-   JOIN film ON children_waiting_list.film_id = film.film_id
-   WHERE dvd_available = FALSE
-   GROUP BY film.title;
+-- 3) Retrieve the number of people waiting for each children’s DVD. 
+	-- Test this by adding rows to the table that you created in question 2 above.
+INSERT INTO waiting_list(complete_name, inventory_id)
+   VALUES('Enfant 1', 1),
+         ('Enfant 2', 2),
+         ('Enfant 3', 3),
+         ('Enfant 4', 4),
+         ('Enfant 5', 5);
+		
+UPDATE waiting_list
+   SET takes = TRUE 
+   WHERE id IN (1, 3, 5);
